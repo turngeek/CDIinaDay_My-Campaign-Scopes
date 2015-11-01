d5344dcb3f3c1fde577b5f5a0c19030df541ff06
Anonymous <anonymous@noemail.com>
2015-11-01 11:01:27 +0000

    init

---
+++ src/main/java/press/turngeek/mycampaign/controller/EditCampaignController.java
@@ -0,0 +1,30 @@
+package press.turngeek.mycampaign.controller;
+
+import press.turngeek.mycampaign.data.CampaignListProducer;
+import press.turngeek.mycampaign.data.CampaignProducer;
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+import java.io.Serializable;
+
+@ViewScoped
+@Named
+public class EditCampaignController implements Serializable {
+	private static final long serialVersionUID = 2815796004558360299L;
+
+	@Inject
+	private CampaignListProducer campaignListProducer;
+	@Inject
+	private CampaignProducer campaignProducer;
+
+	public String doSave() {
+		if (campaignProducer.isAddMode()) {
+			campaignListProducer.getCampaigns().add(
+					campaignProducer.getSelectedCampaign());
+		}
+		return Pages.LIST_CAMPAIGNS;
+	}
+	public String doCancel() {
+		return Pages.LIST_CAMPAIGNS;
+	}
+}
